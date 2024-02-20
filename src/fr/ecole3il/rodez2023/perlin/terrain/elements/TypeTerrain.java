1ea34deca135e2d147f8e0aae65c8ba2cbffeccd
KillianTurmorezoff <turmorek@3il.fr>
2024-02-20 19:45:43 +0100

    feat: ajout de la classe TypeTerrain

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/elements/TypeTerrain.java
@@ -0,0 +1,33 @@
+package fr.ecole3il.rodez2023.perlin.terrain.elements;
+
+import java.awt.image.BufferedImage;
+import fr.ecole3il.rodez2023.perlin.Utils;
+
+public enum TypeTerrain {
+    COLLINES("Collines", "hills.png"),
+    DESERT("Désert", "desert.png"),
+    FORET_CONIFERES("Forêt de conifères", "coniferous_forest.png"),
+    FORET_FEUILLUS("Forêt de feuillus", "deciduous_forest.png"),
+    MARAIS("Marais", "marsh.png"),
+    MONTAGNE("Montagne", "mountain.png"),
+    OCEAN("Océan", "ocean.png"),
+    PLAINE("Plaine", "plain.png"),
+    TOUNDRA("Toundra", "tundra.png");
+
+    private final String nom;
+    private final String fichierImage;
+
+    private TypeTerrain(String nom, String fichierImage) {
+        this.nom = nom;
+        this.fichierImage = fichierImage;
+    }
+
+    public BufferedImage getImage() {
+        return Utils.chargerTuile(fichierImage);
+    }
+
+    @Override
+    public String toString() {
+        return nom;
+    }
+}
