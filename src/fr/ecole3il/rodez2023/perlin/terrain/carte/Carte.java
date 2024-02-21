0386e719e4888a3d8c78ce5c0633b28ba836c265
KillianTurmorezoff <turmorek@3il.fr>
2024-02-21 19:06:26 +0100

    feat: ajout de la classe Carte et l'exception TerrainInexistantException

---
+++ src/fr/ecole3il/rodez2023/perlin/terrain/carte/Carte.java
@@ -0,0 +1,37 @@
+package fr.ecole3il.rodez2023.perlin.terrain.carte;
+
+import fr.ecole3il.rodez2023.perlin.terrain.elements.Terrain;
+import fr.ecole3il.rodez2023.perlin.terrain.generation.GenerateurCarte;
+
+public class Carte {
+    private String nom;
+    private int largeur;
+    private int hauteur;
+    private Terrain[][] terrains;
+
+    public Carte(String nom, int largeur, int hauteur, GenerateurCarte generateurCarte) {
+        this.nom = nom;
+        this.largeur = largeur;
+        this.hauteur = hauteur;
+        this.terrains = generateurCarte.genererCarte(largeur, hauteur);
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public int getLargeur() {
+        return largeur;
+    }
+
+    public int getHauteur() {
+        return hauteur;
+    }
+
+    public Terrain getTerrain(int x, int y) throws TerrainInexistantException {
+        if (x < 0 || x >= largeur || y < 0 || y >= hauteur) {
+            throw new TerrainInexistantException("La case demandée est en dehors des limites de la carte.");
+        }
+        return terrains[x][y];
+    }
+}
